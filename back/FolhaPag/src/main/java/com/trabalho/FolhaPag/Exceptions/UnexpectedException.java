b079c334bd05b3487590fb9098d677fd3f2b93f1
arthurcvl <arthurcvleao@gmail.com>
2025-11-08 18:29:42 -0300

    Create 2 personalized Exceptions

---
+++ back/FolhaPag/src/main/java/com/trabalho/FolhaPag/Exceptions/UnexpectedException.java
@@ -0,0 +1,7 @@
+package com.trabalho.FolhaPag.Exceptions;
+
+public class UnexpectedException extends RuntimeException {
+    public UnexpectedException(String message) {
+        super(message);
+    }
+}
---
+++ back/FolhaPag/src/main/java/com/trabalho/FolhaPag/Exceptions/ValueNotValidException.java
@@ -0,0 +1,7 @@
+package com.trabalho.FolhaPag.Exceptions;
+
+public class ValueNotValidException extends RuntimeException {
+    public ValueNotValidException(String message) {
+        super(message);
+    }
+}
